735c6ebab07779c47e154b0a1876bfb379216666
LeeSangMin1029 <sangmins39@gmail.com>
2021-06-28 20:24:13 +0900

    첫 번째 JSX Editor 컴포넌트 추가

--- src/components/ReactView.js
+++ src/components/ReactView.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Image from './Image.js';
 import { useThemeContext } from './hooks/useTheme.js';
+import JSXEditor from './JSXEditor.js';
 
 const Container = styled.div`
   display: flex;
@@ -105,6 +106,35 @@ const ReactView = () => {
               </p>
             </Description>
           </Section>
+          <Division />
+          <JSXEditor
+            code={`class Counter extends React.Component {
+  constructor() {
+    super()
+    this.state = { count: 0 }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(state => ({ count: state.count + 1 }))
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  render() {
+    return (
+      <center>
+        <h3>
+          {this.state.count}
+        </h3>
+      </center>
+    )
+  }
+}`}
+          />
           <Division />
         </Contents>
       </Container>
