20c80a1c4c43964ef9bfda361a38ce94d3c56b3d
LeeSangMin1029 <sangmins39@gmail.com>
2021-06-28 20:28:08 +0900

    JSX Editor 테스트 추가

---
+++ src/stories/Editor.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import JSXEditor from '../components/JSXEditor.js';
+
+export default {
+  component: JSXEditor,
+  title: 'JSXEditor',
+};
+
+export const Primary = () => <JSXEditor />;
