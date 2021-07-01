abd17294d4bd81f2198a7cc063c31ab981168467
LeeSangMin1029 <sangmins39@gmail.com>
2021-07-01 21:20:59 +0900

    routing 적용

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,3 +1,4 @@
+import { Route } from 'react-router-dom';
 import ReactPage from './react/ReactPage';
 import AngularPage from './angular/AngularPage';
 import VuePage from './vue/VuePage';
@@ -5,9 +6,9 @@ import VuePage from './vue/VuePage';
 const MainContent = () => {
   return (
     <>
-      <ReactPage />
-      <AngularPage />
-      <VuePage />
+      <Route path={['/react', '/']} component={ReactPage} exact />
+      <Route path='/angular' component={AngularPage} exact />
+      <Route path='/vue' component={VuePage} exact />
     </>
   );
 };
